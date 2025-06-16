468d15351eacc0add85ceb5d18219845d850210a
josue.sandoval <josue.sandoval@citi.com.mx>
2025-06-15 23:32:50 -0700

    update

--- k8s-hands-on/aprendizaje/backend/src/main.go
+++ k8s-hands-on/aprendizaje/backend/src/main.go
@@ -7,28 +7,30 @@ import (
 	"time"
 )
 
-// HandsOn representa la estructura que se retornará en JSON
 type HandsOn struct {
 	Time     time.Time `json:"time"`
 	Hostname string    `json:"hostname"`
 }
 
-// ServeHTTP maneja las peticiones GET en la raíz "/"
 func ServeHTTP(w http.ResponseWriter, r *http.Request) {
+
+	if r.URL.Path != "/" {
+		http.NotFound(w, r)
+		return
+	}
+
 	resp := HandsOn{
 		Time:     time.Now(),
 		Hostname: os.Getenv("HOSTNAME"),
 	}
-
 	jsonResp, err := json.Marshal(&resp)
 	if err != nil {
-		w.WriteHeader(http.StatusInternalServerError)
-		w.Write([]byte("Error generating JSON"))
+		w.Write([]byte("Error"))
 		return
 	}
 
 	w.Header().Set("Content-Type", "application/json")
-	w.WriteHeader(http.StatusOK)
+	w.Header().Set("Access-Control-Allow-Origin", "*")
 	w.Write(jsonResp)
 }
 
